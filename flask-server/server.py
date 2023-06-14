765238b9e0a9e45b6259bc1c3a8f8150077355ed
Lasantha Sanjeewa Silva <41699526+sanju2@users.noreply.github.com>
2023-06-14 20:54:48 +0400

    2023-06-14 commit

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/members")
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return {"members": ["Hello", "Erangi", "Hansika"]}
 
 
 if __name__=="__main__":
